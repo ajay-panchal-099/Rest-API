5db401b2dd5fa0629e080458eb15cb8da126feac
Ajay <akpanchal099@gmail.com>
2022-12-26 17:11:53 +0530

    Return Student List as Rest API JSON Response

--- src/main/java/com/api/restapi/controller/StudentC.java
+++ src/main/java/com/api/restapi/controller/StudentC.java
@@ -3,6 +3,9 @@ import com.api.restapi.student.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentC {
 
@@ -10,5 +13,20 @@ public class StudentC {
     @GetMapping("/student")
     public Student getStudent(){
         return new Student("Ram","Shyam");
+    }
+
+
+    //Return List as RestAPI Response
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student("R","M"));
+        students.add(new Student("A","B"));
+        students.add(new Student("D","C"));
+        students.add(new Student("E","F"));
+        students.add(new Student("R","G"));
+
+        return students;
     }
 }
